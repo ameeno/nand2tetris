5540070c58c571f27dbba2cd95c9acb4a92f83ee
Arun Shah <fastah@gmail.com>
2019-06-28 10:18:53 +0100

    Made some further changes to the project

--- MyProjects_Submissions/Project07/Part1_VMTranslator/_VMTranslater.py
+++ MyProjects_Submissions/Project07/Part1_VMTranslator/_VMTranslater.py
@@ -5,44 +5,43 @@ from _CodeWriter import CodeWriter
 from _Parser import Parser
 
 # Translator calls parser and stars
-my_parsed_vm_code = []
-my_input_arg = sys.argv[1]
+_vm_parser_outputs = []
+_input_arg = sys.argv[1]
 
-my_number_of_files = 1
-my_unparsed_vm_code = []
-my_filenames = []
+_number_of_files_to_parse = 1
+_filenames_to_parse = []
 
 translator = Parser()
-if ".vm" in my_input_arg:
-    my_filenames.append(my_input_arg)
+if ".vm" in _input_arg:
+    _filenames_to_parse.append(_input_arg)
 else:
-    if my_input_arg.endswith("/") or my_input_arg.endswith("\\"):
-        my_input_arg = my_input_arg[:-1]
-    for root, dirs, files in os.walk(my_input_arg):
+    if _input_arg.endswith("/") or _input_arg.endswith("\\"):
+        _input_arg = _input_arg[:-1]
+    for root, dirs, files in os.walk(_input_arg):
         for file in files:
             if file.endswith(".vm"):
-                my_number_of_files = my_number_of_files + 1
-                my_filenames.append(file)
+                _number_of_files_to_parse = _number_of_files_to_parse + 1
+                _filenames_to_parse.append(file)
 
-for file in my_filenames:
-    if my_number_of_files > 1:
-        file = my_input_arg + "\\" + file
-    my_parsed_vm_code.append(translator.parse(file))
+for file in _filenames_to_parse:
+    if _number_of_files_to_parse > 1:
+        file = _input_arg + "\\" + file
+    _vm_parser_outputs.append(translator.parse(file))
 
-my_writer = CodeWriter(my_input_arg)
-for file in my_parsed_vm_code:
+vm_to_assembly_writer = CodeWriter(_input_arg)
+for file in _vm_parser_outputs:
     for line in file:
         if "goto" in line or "label" in line:
             # print("Label "+line)
             line = line.split(" ")
-            my_writer.write_label(line[0], line[1])
+            vm_to_assembly_writer.write_label(line[0], line[1])
             pass
         elif " " in line:
             # print("PushPop "+line)
             line = line.split(" ")
-            my_writer.write_push_pop(line[0], line[1], line[2])
+            vm_to_assembly_writer.write_push_pop(line[0], line[1], line[2])
 
         else:
             # print("Arithmetic "+line)
-            my_writer.write_arithmetic(str(line))
-my_writer.close()
+            vm_to_assembly_writer.write_arithmetic(str(line))
+vm_to_assembly_writer.close()
